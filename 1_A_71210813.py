d5dc37cb74b15d7837fa1ab92c6c84a9e283fd65
Billie Steven <billie.steven@ti.ukdw.ac.id>
2021-11-12 09:21:07 +0700

    Program sederhana untuk menghitung keliling lingkaran

---
+++ 1_A_71210813.py
@@ -0,0 +1,18 @@
+#Program sederhana untuk menghitung keliling lingkaran!
+#Dengan inputan
+print("")
+print("Test case 1")
+r = int(input("Masukkan diameter : "))
+
+pi = 22/7
+keliling = pi * r
+
+print("Keliling lingkaran adalah :","%.1f" % keliling)
+print("")
+print("Test case 2")
+r = int(input("Masukkan diameter : "))
+
+pi = 22/7
+keliling = pi * r
+
+print("Keliling lingkaran adalah :","%.2f" % keliling)
