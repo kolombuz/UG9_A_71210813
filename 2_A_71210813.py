6cc720586abd08591f99995a24cc6c95334fee75
Billie Steven <billie.steven@ti.ukdw.ac.id>
2021-11-12 09:21:50 +0700

    Program sederhana yang bisa menghitung perpangkatan 5

---
+++ 2_A_71210813.py
@@ -0,0 +1,16 @@
+#Program sederhana yang bisa menghitung perpangkatan 5!
+#Dengan inputan
+print("")
+print ("Test case 1")
+angka = int(input('Masukkan angka: '))
+pangkat = 5
+
+hasil = angka ** pangkat
+print(f'{angka} pangkat {pangkat} adalah {hasil}')
+print ("")
+print ("Test case 2")
+angka = int(input('Masukkan angka: '))
+pangkat = 5
+
+hasil = angka ** pangkat
+print(f'{angka} pangkat {pangkat} adalah {hasil}')
