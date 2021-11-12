4daed0d9692a40b3cb3757a2e0912dad3207f173
Billie Steven <billie.steven@ti.ukdw.ac.id>
2021-11-12 09:23:00 +0700

    Fitur untuk menambah catatan di program Doni

---
+++ 3_A_71210813.py
@@ -0,0 +1,20 @@
+#Fitur untuk menambah catatan di program Doni!
+#Dengan inputan
+print("")
+print ("Test Case 1")
+belanja1 = (input('Masukkan daftar belanja 1: '))
+belanja2 = (input('Masukkan daftar belanja 2: '))
+Tambahkan1 = (input('Tambahkan data ke daftar belanja 1: '))
+Tambahkan2 = (input('Tambahkan data ke daftar belanja 2: '))
+
+print(f'Daftar belanja 1 adalah {belanja1},{Tambahkan1}')
+print(f'Daftar belanja 2 adalah {belanja2},{Tambahkan2}')
+print("")
+print ("Test Case 2")
+belanja1 = (input('Masukkan daftar belanja 1: '))
+belanja2 = (input('Masukkan daftar belanja 2: '))
+Tambahkan1 = (input('Tambahkan data ke daftar belanja 1: '))
+Tambahkan2 = (input('Tambahkan data ke daftar belanja 2: '))
+
+print(f'Daftar belanja 1 adalah {belanja1},{Tambahkan1}')
+print(f'Daftar belanja 2 adalah {belanja2},{Tambahkan2}')
